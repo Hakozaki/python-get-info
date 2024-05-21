64af8d210160363fd5a9cb33076aa2b26d66b708
Marcus Hakozaki <marcus.hakozaki@gmail.com>
2024-05-21 14:59:16 -0500

    Adiona metodo de tratamento de dados

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from download import download
+from flask import Flask , send_file
+from download import coletar_dados
+from tratamento import tratar_dados
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    download()
-    return "<p>Hello, World!</p>"
+    coletar_dados()
+    tratar_dados()
+    return send_file('dados_sefaz_ac_soma.csv', as_attachment=True)
